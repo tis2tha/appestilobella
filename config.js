5b591aa89efcfe8f2193de5f35fe85b6cb8c8a68
tis2tha <33001546+tis2tha@users.noreply.github.com>
2019-05-04 19:55:27 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'estilobella',
+		wp_ws_url : 'http://estilobella.com.br/wp-appkit-api/estilobella',
+		wp_url : 'http://estilobella.com.br',
+		theme : 'q-android',
+		version : '1001',
+		app_type : 'phonegap-build',
+		app_title : '',
+		app_platform : 'android',
+		app_path: '',
+		gmt_offset : -3,
+		debug_mode : 'off',
+		auth_key : 'q~V_/c{cIy4kdn84}i43:#$*Xz7wX/XD efT=[>(S<MJp4Jk>eit7j,n*SS<]Y*=',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
